2c6d15e463d369e3a2ebd5711ce772c30ade1ac0
Kristofer Reynolds <kristofertreynolds@gmail.com>
2026-02-14 01:40:32 +0000

    nested for loop

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  // create combinations of strings using arrays
+
+  const colors = ["black", "red", "white", "green", "pink"];
+  const animals = ["wolf", "chicken", "panther", "crocodile"];
+  const locations = ["swamp", "tundra", "jungle", "desert"];
+
+  for(let colorIndex = 0; colorIndex < colors.length; colorIndex++){
+    for(let animalsIndex = 0; animalsIndex < animals.length; animalsIndex++){
+      for (let locationIndex = 0; locationIndex < locations.length; locationIndex++){
+        console.log(colors[colorIndex] + animals[animalsIndex] + locations[locationIndex] + ".com");
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
 };
