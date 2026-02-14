62b2fe4be5acc5a3adaa256832b276355391f878
Kristofer Reynolds <kristofertreynolds@gmail.com>
2026-02-14 01:55:54 +0000

    nested for in loops

--- src/app.js
+++ src/app.js
@@ -10,16 +10,31 @@ window.onload = function() {
   
   // create combinations of strings using arrays
 
-  const colors = ["black", "red", "white", "green", "pink"];
-  const animals = ["wolf", "chicken", "panther", "crocodile"];
-  const locations = ["swamp", "tundra", "jungle", "desert"];
+  // const colors = ["black", "red", "white", "green", "pink"];
+  // const animals = ["wolf", "chicken", "panther", "crocodile"];
+  // const locations = ["swamp", "tundra", "jungle", "desert"];
 
-  for(let colorIndex = 0; colorIndex < colors.length; colorIndex++){
-    for(let animalsIndex = 0; animalsIndex < animals.length; animalsIndex++){
-      for (let locationIndex = 0; locationIndex < locations.length; locationIndex++){
-        console.log(colors[colorIndex] + animals[animalsIndex] + locations[locationIndex] + ".com");
+  // for(let colorIndex = 0; colorIndex < colors.length; colorIndex++){
+  //   for(let animalsIndex = 0; animalsIndex < animals.length; animalsIndex++){
+  //     for (let locationIndex = 0; locationIndex < locations.length; locationIndex++){
+  //       console.log(colors[colorIndex] + animals[animalsIndex] + locations[locationIndex] + ".com");
+  //     }
+  //   }
+  // }
+
+  let pronoun = ['the', 'our', 'she', 'he'];
+  let adjective = ['great', 'big', 'awesome', 'giant'];
+  let noun = ['jogger', 'racoon', 'man', 'woman'];
+
+  for(let i in pronoun){
+    //console.log(pronoun[index]);
+    for(let j in adjective){
+      //console.log(pronoun[key] + adjective[key]);  
+      for(let k in noun){
+        console.log(pronoun[i] + adjective[j] + noun[k] + ".com");  
       }
     }
+
   }
 
 
